f776a1953eba8335c1bef9d89ff978917ed875a9
icedkungbot <132274296+icedkungbot@users.noreply.github.com>
2023-05-29 21:50:31 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ let activeUsers = [];
 
 const io = require("socket.io")(httpServer, {
     cors: {
-        origins: ["http://localhost:8080"]
+        origins: ["http://localhost:8080","https://tiget-roomchat.onrender.com/","*",]
     },
     handlePreflightRequest: (req, res) => {
         const headers = {
